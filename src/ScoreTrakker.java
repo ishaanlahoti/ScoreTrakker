c8478d6c274a52cb7e64c0e1baf5dd049c4d7546
Ishaan Lahoti <ilahoti@mines.edu>
2023-09-25 19:25:20 -0600

    Finished, waiting for review from Caleb - Ishaan

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -6,9 +6,12 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class ScoreTrakker {
-	private static ArrayList<Student> students = new ArrayList<Student>();
 	
-	public static void loadDataFile(String inputFile) {
+	private static ArrayList<Student> students;
+	private String[] files = {"scores.txt", "badscore.txt", "nofile.txt" };
+	
+	public static void loadDataFile(String inputFile) throws FileNotFoundException {
+		students = new ArrayList<Student>();
 		FileReader file = null;
 		Boolean fileFound = false;
 		
@@ -16,12 +19,24 @@ public class ScoreTrakker {
 			file = new FileReader(inputFile);
 		}
 		catch(FileNotFoundException f) {
-			System.out.println("Can not open file: " + inputFile);
+			throw f;
 		}
+		
 		Scanner scan = new Scanner(file);
+		
 		while(scan.hasNext()) {
 			String name = scan.nextLine();
-			int score = Integer.parseInt(scan.nextLine());
+			String line = scan.nextLine();
+			int score = 0;
+			
+			try {
+				score = Integer.parseInt(line);
+			}
+			catch(NumberFormatException n) {
+				System.out.println("Incorrect format for " + name + " not a valid score: " + line + "\n");
+				continue;
+			}
+			
 			Student student = new Student(name, score);
 			students.add(student);
 		}
@@ -30,19 +45,27 @@ public class ScoreTrakker {
 	
 	public void printInOrder() {
 		Collections.sort(students);
+		System.out.println("Student Score List");
 		for(Student s: students) {
 			System.out.println(s);
 		}
+		System.out.println();
 	}
 	
 	public void processFiles() {
-		loadDataFile("scores.txt");
-		printInOrder();
+		for(String s: files) {
+			try {
+				loadDataFile(s);
+				printInOrder();
+			} catch (FileNotFoundException f) {
+				System.out.println("Can not open file: " + s);
+			}
+
+		}
 	}
 
 	public static void main(String[] args) {
 		ScoreTrakker tracker = new ScoreTrakker();
 		tracker.processFiles();
 	}
-
 }
